6a108b672c34d0a14ba2a1fec80b25cf753bbdc3
Pham Cao Dai <daikadai1026@gmail.com>
2021-01-04 15:32:46 +0700

    3. Routes Autoloading

--- server/server.js
+++ server/server.js
@@ -1,36 +1,35 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+require("dotenv").config();
 
-//import routes
-const authRoutes = require('./routes/auth')
-
-//app 
-const app = express()
+//app
+const app = express();
 
 //db
-mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('DB CONNECTED'))
-.catch(err => console.log(err))
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log(err));
 
 //middlewares
-app.use(morgan('dev'))
-app.use(bodyParser.json({ limit: '2mb'}))
-app.use(cors())
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
 
 //Routes middleware
-app.use('/api', authRoutes)
+readdirSync("./routes").map((r) =>
+  app.use("/api", require("./routes/" + r))
+);
+//port
+const port = process.env.PORT || 8000;
 
-//port 
-const port = process.env.PORT || 8000
-
-app.listen(port, () => console.log(`Server is running on port ${port}`))
-
+app.listen(port, () => console.log(`Server is running on port ${port}`));
